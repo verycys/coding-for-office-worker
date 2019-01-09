4164046880e2e29efc766009eaf7b8a9dc6f548d
Yunseok Choi <yunseokchoi@Yunseokui-MacBook-Pro.local>
2019-01-09 17:55:27 +0900

    Add - Learn conversation - Learn int, floot, str

---
+++ 02-data-type-1.py
@@ -0,0 +1,22 @@
+# 정수 int - intiger
+print(1+1)
+# 소수 float
+print(1.5+2.1)
+#문자 str - string
+print("hello")
+
+# 문자열 더하기
+print("hello"+"1")
+
+# 정수 소수 간 변경
+print(4/2)
+print(4//2)
+print(int(4/2))
+print(float(2))
+
+# 소수를 정수로 출력하기
+print(int("1.5"))
+# error가 출력됨, 파이썬 개발자가 임의로 설정한 것임
+
+print(int(1.5))
+# 1이 출력됨
